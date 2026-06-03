7148f28e76d887cd98ffe0843533cb9fbe7e2dfc
dheyb <72129719+dheyb@users.noreply.github.com>
2026-06-03 19:55:39 +0800

    fix: base path to root for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/dheybg/", // <-- Changed this to match your repository name
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
